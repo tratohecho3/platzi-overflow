41dae85884d6fdd564476ef366b28d5e69a31afa
Cesar <tratohecho3@gmail.com>
2018-05-22 18:39:31 -0400

    question detail with params in url implemented

--- src/app/question-detail/question-detail.component.ts
+++ src/app/question-detail/question-detail.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Question } from './question.model';
-
+import { QuestionService } from '../services/question.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-question-detail',
   templateUrl: './question-detail.component.html',
   styleUrls: ['./question-detail.component.css']
 })
-export class QuestionDetailComponent implements OnInit {
+export class QuestionDetailComponent implements OnInit, OnDestroy {
 
-  question: Question = new Question('Esta es una nueva pregunta sobre Android',
-                                    'Miren, tengo una duda con una aplicacion que estoy escribiendo para Android...',
-                                    new Date(),
-                                    'devicon-android-plain');
+  question?: Question;
+  loading = true;
+  sub: any;
 
-  constructor() { }
+  constructor(private questionService: QuestionService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.questionService.getQuestion(params.id)
+        .then((question: Question) => {
+          this.question = question;
+          this.loading = false;
+        });
+    });
+    
+  }
+
+  ngOnDestroy(){
+    this.sub.unsubscribe();
   }
 
 }
